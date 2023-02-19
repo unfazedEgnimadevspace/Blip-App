a1fc615079c636c390f78e989406f1d14106428e
Akalugo Daniel <akalugidaniel@gmail.com>
2023-02-19 22:15:13 +0100

    Added a status feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -88,6 +88,24 @@ class UserTest < ActiveSupport::TestCase
     assert archer.followers.include?(micheal)
     micheal.unfollow(archer)
     assert_not micheal.following?(archer)
-
     end
+
+    test "feed should have the right posts" do
+      micheal = users(:micheal)
+      archer = users(:archer)
+      lana = users(:lana)
+      # Posts from followed user
+      lana.microposts.each do |post_following|
+      assert micheal.feed.include?(post_following)
+      end
+      # Posts from self
+      micheal.microposts.each do |post_self|
+      assert micheal.feed.include?(post_self)
+      end
+      # Posts from unfollowed user
+      archer.microposts.each do |post_unfollowed|
+      assert_not micheal.feed.include?(post_unfollowed)
+      end
+    end
+      
 end
