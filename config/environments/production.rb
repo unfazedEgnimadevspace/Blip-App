0180353e5b3bc690ecc0b62a17e63af3eb259e5f
Akalugo Daniel <akalugidaniel@gmail.com>
2023-02-28 14:08:35 +0100

    still fixing production bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
- 
+  host = 'https://blip-app-4.onrender.com' #replace with your own url
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
